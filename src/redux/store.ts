4c7101115f269f60daf923209b5bf0d74ee40d44
Matiwos Desalegn <77539602+matidesalegn@users.noreply.github.com>
2024-09-17 23:57:09 +0300

    "Update @reduxjs/toolkit version, remove thunk import, and modify tsconfig.json"

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
-import thunk from "redux-thunk"; // Remove 'import thunk from redux-thunk' from JS if toolkit used
-import rootReducer from "./Reducer/root-reducer";
+import rootReducer from "./Reducer/root-reducer"; // Assuming you have a root reducer
 
 const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk, logger),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(logger), // No need to manually add thunk
 });
 
 export default store;
